c68e49da823d923d749ea0a2cc593a0f1a2f9f61
Sebastian <55423389+Sebastian-RP@users.noreply.github.com>
2024-11-08 11:06:24 -0500

    documentation with Swagger working

--- src/DTO/createDeliveryDto.ts
+++ src/DTO/createDeliveryDto.ts
@@ -1,22 +1,43 @@
 import { IsString, IsNotEmpty, IsNumber, IsDateString } from 'class-validator';
-import { Transform } from 'class-transformer';
+import { ApiProperty } from '@nestjs/swagger';  // Importar el decorador de Swagger
 
 export class CreateDeliveryDto {
+  
+    @ApiProperty({
+        description: 'El destinatario del envío',
+        example: 'Juan Pérez',  // Un ejemplo del valor esperado
+    })
     @IsString({ message: 'El campo destinatario debe ser un texto' })
     @IsNotEmpty({ message: 'El campo destinatario es requerido' })
     destinatario: string;
 
+    @ApiProperty({
+        description: 'El remitente del envío',
+        example: 'Maria López',
+    })
     @IsString({ message: 'El campo remitente debe ser un texto' })
     @IsNotEmpty({ message: 'El campo remitente es requerido' })
     remitente: string;
 
+    @ApiProperty({
+        description: 'El contenido del paquete que se va a enviar',
+        example: 'Ropa',
+    })
     @IsString({ message: 'El campo contenido debe ser un texto' })
     @IsNotEmpty({ message: 'El campo contenido es requerido' })
     contenido: string;
 
+    @ApiProperty({
+        description: 'La fecha en la que se realiza el envío (formato ISO)',
+        example: '2024-11-25',  // Ejemplo de fecha en formato ISO
+    })
     @IsDateString({}, { message: 'La fecha debe tener un formato valido, ejemplo: 2024-11-25' })
     fecha_envio: string;
 
+    @ApiProperty({
+        description: 'La distancia en kilómetros entre el remitente y el destinatario',
+        example: 150,  // Ejemplo de distancia
+    })
     @IsNumber({}, { message: 'El campo distancia debe ser un número' })
     @IsNotEmpty({ message: 'El campo distancia es requerido' })
     distancia: number;
